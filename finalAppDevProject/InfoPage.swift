e88280451bb4baebc4112e0abf93a89f5de6a398
PaigeDinh <Paigedinh2008@gmail.com>
2022-08-04 08:28:14 -0700

    Link

---
+++ finalAppDevProject/InfoPage.swift
@@ -0,0 +1,31 @@
+//
+//  InfoPage.swift
+//  finalAppDevProject
+//
+//  Created by Paige Dinh on 8/4/22.
+//
+
+import UIKit
+
+class InfoPage: UIViewController {
+
+    @IBAction func Link1(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
