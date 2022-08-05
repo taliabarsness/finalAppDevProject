96f5dc11c26e204ef80a926e899b315be2a72b5d
PaigeDinh <Paigedinh2008@gmail.com>
2022-08-05 01:19:29 -0700

    m

--- finalAppDevProject/InfoPage.swift
+++ finalAppDevProject/InfoPage.swift
@@ -18,68 +18,68 @@ class InfoPage: UIViewController {
     
     
     @IBAction func Link2(_ sender: Any) {
-        if let url = URL(string: "https://corporate.exxonmobil.com") {
+        if let url = URL(string: "https://europa.eu/youth/get-involved/sustainable-development/how-reduce-my-carbon-footprint_en") {
               UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     @IBAction func Link3(_ sender: Any) {
-        if let url = URL(string: "https://corporate.exxonmobil.com") {
+        if let url = URL(string: "https://news.climate.columbia.edu/2018/12/27/35-ways-reduce-carbon-footprint/") {
               UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     @IBAction func Link4(_ sender: Any) {
-        if let url = URL(string: "https://corporate.exxonmobil.com") {
+        if let url = URL(string: "https://cotap.org/reduce-carbon-emissions/") {
               UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     
     @IBAction func Link5(_ sender: Any) {
-        if let url = URL(string: "https://corporate.exxonmobil.com") {
+        if let url = URL(string: "https://www.nytimes.com/guides/year-of-living-better/how-to-reduce-your-carbon-footprint") {
               UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     
     @IBAction func Link6(_ sender: Any) {
-        if let url = URL(string: "https://corporate.exxonmobil.com") {
+        if let url = URL(string: "https://www.wired.co.uk/article/reduce-carbon-footprint") {
               UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     
     @IBAction func Link7(_ sender: Any) {
-        if let url = URL(string: "https://corporate.exxonmobil.com") {
+        if let url = URL(string: "https://www.epa.gov/ghgemissions/sources-greenhouse-gas-emissions") {
               UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     
     @IBAction func Link8(_ sender: Any) {
-        if let url = URL(string: "https://corporate.exxonmobil.com") {
+        if let url = URL(string: "https://www.nature.org/en-us/get-involved/how-to-help/carbon-footprint-calculator/") {
               UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     
     @IBAction func Link9(_ sender: Any) {
-        if let url = URL(string: "https://corporate.exxonmobil.com") {
+        if let url = URL(string: "https://www.ers.usda.gov/topics/natural-resources-environment/climate-change/") {
               UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     
     @IBAction func Link10(_ sender: Any) {
-        if let url = URL(string: "https://corporate.exxonmobil.com") {
+        if let url = URL(string: "https://ourworldindata.org/co2-and-other-greenhouse-gas-emissions") {
               UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     
     @IBAction func LInk11(_ sender: Any) {
-        if let url = URL(string: "https://corporate.exxonmobil.com") {
+        if let url = URL(string: "https://climate.nasa.gov/vital-signs/carbon-dioxide/") {
               UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
